02b53177d47244162069ffce000346fe47ac8065
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-03 22:12:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :theme 
       t.float :price
       t.boolean :family_friendly
-      t.string :opening_time
-      t.string :closing_time
+      t.date :opening_time
+      t.date :closing_time
       end
   end
 end 
