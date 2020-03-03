9a2f220a24a7c124bef5a00651f3d540f5ce6c8e
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-03 22:08:09 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,16 +1,4 @@
 # Create your HauntedHouse class here
 class HauntedHouse < ActiveRecord::Base
-  class CreateCostumeStores < ActiveRecord::Migration[5.2]
-  def change 
-    create_table :haunted_houses, :force => true do |t|
-      t.string :name
-      t.string :location
-      t.string :theme 
-      t.float :price
-      t.boolean :still_in_business
-      t.string :opening_time
-      t.string :closing_time
-      end
-  end
-end 
+  
 end
