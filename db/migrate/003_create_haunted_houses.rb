23fadb5ab5b4cb66999d48c9ccee48e2421f1c5e
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-03 22:08:41 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change 
+    create_table :haunted_houses, :force => true do |t|
+      t.string :name
+      t.string :location
+      t.string :theme 
+      t.float :price
+      t.boolean :family_friendly
+      t.string :opening_time
+      t.string :closing_time
+      end
+  end
+end 
